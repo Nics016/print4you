4aa26a4392b7edf9352ff2990f2706f18343f24f
Nikita <nics009@yandex.ru>
2017-05-18 21:02:21 +0300

    Added previously ignored files

---
+++ common/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'language' => 'ru',
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=localhost;port=5432;dbname=print4you',
+            'username' => 'postgres',
+            'password' => '333777aaa',
+            'charset' => 'UTF8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+    ],
+];
